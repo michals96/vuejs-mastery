3a48e2ba83a583b2590bd1f5b3329b4aafcb2b7c
michals96 <michal.stefaniuk9@gmail.com>
2023-03-13 14:32:04 +0100

    Update tasks

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -49,8 +49,20 @@ export default {
         res.status === 200 ? this.tasks = this.tasks.filter(task => task.id !== id) : alert("Error deleting task")
       }
     },
-    toggleReminder(id) {
-      this.tasks = this.tasks.map(task => task.id === id ? {...task, reminder: !task.reminder} : task);
+    async toggleReminder(id) {
+      const taskToToggle = await this.fetchTask(id);
+      const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+      const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(updTask)
+      })
+
+      const data = await res.json();
+
+      this.tasks = this.tasks.map(task => task.id === id ? {...task, reminder: data.reminder} : task);
     },
     toggleAddTask() {
       this.showAddTask = !this.showAddTask
